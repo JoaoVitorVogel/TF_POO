f137462b36ddd05ccd9643403a5b621a47cb32bd
Daniel Martins <danicoxa.martins@outlok.com>
2023-11-13 12:45:53 -0300

    contMenus

--- Ferroviaria/src/main/java/org/example/InterfaceGrafica/JanelaPrincipal.java
+++ Ferroviaria/src/main/java/org/example/InterfaceGrafica/JanelaPrincipal.java
@@ -12,42 +12,52 @@ public class JanelaPrincipal extends JFrame{
         // Opção ver garagens
         JButton botaoVerGaragens = new JButton("Ver garagens");
         botaoVerGaragens.addActionListener(evento -> {
-            JanelaGaragens janelaGaragens = new JanelaGaragens();
-            janelaGaragens.setSize(400, 300);
-            janelaGaragens.setLocationRelativeTo(null);
-            janelaGaragens.setVisible(true);
-        });
+                JanelaGaragens janelaGaragens = new JanelaGaragens();
+                janelaGaragens.setSize(500, 300);
+                janelaGaragens.setLocationRelativeTo(null);
+                janelaGaragens.setVisible(true);
+            }
+        );
 
         // Opção criar trem
         JButton botaoCriarTrem = new JButton("Criar trem");
         botaoCriarTrem.addActionListener(evento -> {
-            JanelaCriaTrem janelaCriaTrem = new JanelaCriaTrem();
-            janelaCriaTrem.setSize(400, 300);
-            janelaCriaTrem.setLocationRelativeTo(null);
-            janelaCriaTrem.setVisible(true);
-        });
+                JanelaCriaTrem janelaCriaTrem = new JanelaCriaTrem();
+                janelaCriaTrem.setSize(500, 300);
+                janelaCriaTrem.setLocationRelativeTo(null);
+                janelaCriaTrem.setVisible(true);
+            }
+        );
 
          // Opção editar um trem
         JButton botaoEditarTrem = new JButton("Editar o trem");
         botaoEditarTrem.addActionListener(evento -> {
-            JanelaEditTrem janelaEditTrem = new JanelaEditTrem();
-            janelaEditTrem.setSize(400, 300);
-            janelaEditTrem.setLocationRelativeTo(null);
-            janelaEditTrem.setVisible(true);
-        });
-        add(botaoEditarTrem);
+                JanelaEditTrem janelaEditTrem = new JanelaEditTrem();
+                janelaEditTrem.setSize(500, 300);
+                janelaEditTrem.setLocationRelativeTo(null);
+                janelaEditTrem.setVisible(true);
+            }
+        );
 
         // Desfazer o trem
         JButton botaoDesfazOTrem = new JButton("Desfazer o trem");
         botaoDesfazOTrem.addActionListener(evento -> {
-            JanelaDesfazTrem janelaDesfazTrem = new JanelaDesfazTrem();
-            janelaDesfazTrem.setSize(400, 300);
-            janelaDesfazTrem.setLocationRelativeTo(null);
-            janelaDesfazTrem.setVisible(true);
-        });
+                JanelaDesfazTrem janelaDesfazTrem = new JanelaDesfazTrem();
+                janelaDesfazTrem.setSize(500, 300);
+                janelaDesfazTrem.setLocationRelativeTo(null);
+                janelaDesfazTrem.setVisible(true);
+            }
+        );
+
+        // Salvar alterações
+        JButton botaoSalvar = new JButton("Salvar e sair");
+        botaoSalvar.addActionListener(evento -> {
+            //
+            }
+        );
 
         // Monta painel
-        JPanel painel = new JPanel(new GridLayout(5,2));
+        JPanel painel = new JPanel(new GridLayout(4,2));
         painel.add(botaoVerGaragens);
         painel.add(botaoCriarTrem);
         painel.add(botaoEditarTrem);
